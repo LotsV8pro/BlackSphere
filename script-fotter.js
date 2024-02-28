fcd4d8b7e3552546f9602a2968d7046b56d89e84
Albert Romero <lotsv8proyt@gmail.com>
2024-02-28 20:51:32 +0100

    Aregllado Fotter
    ahora si aparece ne la pagina rpincipal

--- script-fotter.js
+++ script-fotter.js
@@ -1,10 +1,19 @@
-window.addEventListener('scroll', function() {
-    var footer = document.querySelector('footer');
-    var scrollPosition = window.scrollY;
+document.addEventListener("DOMContentLoaded", function() {
+    var isScrollable = document.body.scrollHeight > window.innerHeight;
 
-    if (scrollPosition > 100) { // Cambia este valor según sea necesario
-        footer.classList.add('show-footer');
+    if (isScrollable) {
+        window.addEventListener('scroll', function() {
+            var footer = document.querySelector('footer');
+            var scrollPosition = window.scrollY;
+
+            if (scrollPosition > 100) { // Cambia este valor según sea necesario
+                footer.classList.add('show-footer');
+            } else {
+                footer.classList.remove('show-footer');
+            }
+        });
     } else {
-        footer.classList.remove('show-footer');
+        var footer = document.querySelector('footer');
+        footer.classList.add('show-footer');
     }
 });
