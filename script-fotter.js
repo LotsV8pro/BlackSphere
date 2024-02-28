b8cdbb72f5acc8b86ebe5806b16fcd8dac85a818
Albert Romero <lotsv8proyt@gmail.com>
2024-02-28 20:47:09 +0100

    Fotter en form arreglado
    Ya tiene animacion

--- script-fotter.js
+++ script-fotter.js
@@ -1,7 +1,8 @@
 window.addEventListener('scroll', function() {
     var footer = document.querySelector('footer');
-    var scrollPosition = window.scrollY + window.innerHeight;
-    if (scrollPosition >= document.body.scrollHeight) {
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) { // Cambia este valor según sea necesario
         footer.classList.add('show-footer');
     } else {
         footer.classList.remove('show-footer');
