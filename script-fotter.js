9eb279b87bbe57a405b067535b32336f082ca3a5
Albert Romero <lotsv8proyt@gmail.com>
2024-02-29 17:09:31 +0100

    Actualizado el JS para funcionar en telf correctamente
    Ademas de actualizar el js ahora cuando la resolucion es de telf y als tarjetas no entran a lo ancho se pondran en vertical

--- script-fotter.js
+++ script-fotter.js
@@ -1,5 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    function adjustFooter() {
+    // Función para ajustar el footer en dispositivos móviles
+    function adjustFooterMobile() {
+        var footer = document.querySelector('footer');
+        var content = document.querySelector('.content');
+        var isScrollable = document.body.scrollHeight > window.innerHeight;
+
+        if (isScrollable) {
+            var scrollPosition = window.scrollY;
+
+            // Calculamos la distancia mínima para mostrar el footer sin superponer el contenido
+            var minScrollToShowFooter = document.body.scrollHeight - window.innerHeight;
+
+            if (scrollPosition >= minScrollToShowFooter) {
+                footer.classList.add('show-footer');
+            } else {
+                footer.classList.remove('show-footer');
+            }
+        } else {
+            footer.classList.add('show-footer');
+        }
+
+        // Ajustar margen inferior del contenido solo si el footer no está fijo al final de la página
+        if (!footer.classList.contains('fixed-footer')) {
+            var footerHeight = footer.offsetHeight;
+            content.style.marginBottom = footerHeight + 'px';
+        }
+    }
+
+    // Función para ajustar el footer en dispositivos no móviles
+    function adjustFooterDesktop() {
         var footer = document.querySelector('footer');
         var isScrollable = document.body.scrollHeight > window.innerHeight;
 
@@ -16,8 +45,15 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
-    adjustFooter(); // Llamar a la función para ajustar el footer cuando se carga la página
-
-    window.addEventListener('scroll', adjustFooter);
-    window.addEventListener('resize', adjustFooter);
+    // Agregar clase al body si es un dispositivo móvil
+    if (/Mobi|Android/i.test(navigator.userAgent)) {
+        document.body.classList.add('mobile');
+        adjustFooterMobile(); // Llamar a la función para ajustar el footer en dispositivos móviles
+        window.addEventListener('scroll', adjustFooterMobile);
+        window.addEventListener('resize', adjustFooterMobile);
+    } else {
+        adjustFooterDesktop(); // Llamar a la función para ajustar el footer en dispositivos no móviles
+        window.addEventListener('scroll', adjustFooterDesktop);
+        window.addEventListener('resize', adjustFooterDesktop);
+    }
 });
